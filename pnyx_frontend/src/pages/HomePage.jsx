fae92f366e8622f5fe5e1113d8decc3fb1a8a8f9
Nathaniel Aquino <nataquino29@gmail.com>
2024-06-17 12:33:35 +0800

    Modified Survey Listing

--- pnyx_frontend/src/pages/HomePage.jsx
+++ pnyx_frontend/src/pages/HomePage.jsx
@@ -1,33 +1,12 @@
-import {
-    Stack,
-    Box,
-    Stepper,
-    Step,
-    StepLabel,
-    Container,
-    Typography,
-    TextField,
-    Button,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-} from "@mui/material";
-import { useState } from "react";
-import * as React from "react";
-import { useNavigate } from "react-router-dom";
 
+import { Stack} from "@mui/material";
+import NavBar from "../components/NavBar";
 
 const HomePage = () => {
-    return(
-    <Stack
-        sx={{
-        backgroundColor: "skyblue",
-        height: "110vh",
-        }}
-    >
-        <Typography sx={{ marginTop: "40px", fontSize: "30px" }}>Welcome Home :D</Typography>
+  return (
+    <Stack fullWidth sx={{ backgroundColor: "skyblue", height: "100vh" }}>
+      <NavBar />
     </Stack>
-    );
-}
+  );
+};
 export default HomePage;
