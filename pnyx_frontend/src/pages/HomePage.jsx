09aeabb320cb415843378abfe030f293b699b396
Jerzeil <72914327+Jerzeil@users.noreply.github.com>
2024-12-15 16:13:22 +0800

    Update HomePage.jsx

--- pnyx_frontend/src/pages/HomePage.jsx
+++ pnyx_frontend/src/pages/HomePage.jsx
@@ -1,4 +1,13 @@
-import { Stack, Box, Container, Card, CardContent, Typography, Button, CardActions } from "@mui/material";
+import {
+  Stack,
+  Box,
+  Container,
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  CardActions,
+} from "@mui/material";
 import NavBar from "../components/NavBar";
 import { Link, useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
@@ -12,7 +21,10 @@ const HomePage = () => {
   useEffect(() => {
     const fetchSurveys = async () => {
       try {
-        const response = await axios.get("http://localhost/survey-app/get-approved-surveys.php", { withCredentials: true });
+        const response = await axios.get(
+          "http://localhost/survey-app/get-approved-surveys.php",
+          { withCredentials: true }
+        );
         console.log(response); // Log the entire response
         console.log(response.data); // Log the response data
         console.log(Array.isArray(response.data)); // Check if response.data is an array
@@ -36,8 +48,8 @@ const HomePage = () => {
   // Handle logout
   const handleLogout = () => {
     // Delete specific cookies
-    deleteCookie('username');   
-    deleteCookie('user_id');
+    deleteCookie("username");
+    deleteCookie("user_id");
 
     // Navigate to the login or signup page
     navigate("/"); // Change to your desired route
@@ -54,67 +66,69 @@ const HomePage = () => {
           alignItems: "center", // Center cards horizontally
           marginTop: "20px",
           paddingBottom: 5,
+          marginLeft: -15
         }}
       >
-        <Box>
-          
-        </Box>
-          <Container spacing={9} sx={{ marginTop: 5, marginBottom: 4, flexGrow: 1, backgroundColor: "red" }}>
-            {surveys.length === 0 && (
-              <Typography variant="h6" color="error">
-                No surveys found.
-              </Typography>
-            )}
-            <Stack spacing={4}> {/* Stack cards vertically with spacing */}
-              {surveys.map((survey) => (
-                <Card
-                  key={survey.id}
+        <Container
+          spacing={9}
+          sx={{ marginTop: 5, marginBottom: 4, flexGrow: 1 }}
+        >
+          {surveys.length === 0 && (
+            <Typography variant="h6" color="error">
+              No surveys found.
+            </Typography>
+          )}
+          <Stack spacing={4}>
+            {" "}
+            {/* Stack cards vertically with spacing */}
+            {surveys.map((survey) => (
+              <Card
+                key={survey.id}
+                sx={{
+                  width: "100%",
+                  maxWidth: 600, // Make card responsive with max width
+                  height: "auto", // Make card height flexible
+                  display: "flex",
+                  flexDirection: "column",
+                  borderRadius: 5,
+                }}
+              >
+                <CardContent>
+                  <Typography
+                    variant="h5"
+                    component="div"
+                    sx={{ marginTop: 2, fontSize: "30px" }}
+                  >
+                    {survey.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{ marginTop: 2 }}
+                  >
+                    {survey.description}
+                  </Typography>
+                </CardContent>
+                <CardActions
                   sx={{
-                    width: "100%",
-                    maxWidth: 600, // Make card responsive with max width
-                    height: "auto", // Make card height flexible
+                    marginTop: "auto",
                     display: "flex",
-                    flexDirection: "column",
-                    borderRadius: 5,
+                    justifyContent: "flex-end",
                   }}
                 >
-                  <CardContent>
-                    <Typography
-                      variant="h5"
-                      component="div"
-                      sx={{ marginTop: 2, fontSize: "30px" }}
-                    >
-                      {survey.title}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{ marginTop: 2 }}
-                    >
-                      {survey.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions
-                    sx={{
-                      marginTop: "auto",
-                      display: "flex",
-                      justifyContent: "flex-end",
-                    }}
+                  <Button
+                    size="small"
+                    color="primary"
+                    component={Link}
+                    to={`/take-survey/${survey.id}`}
                   >
-                    <Button
-                      size="small"
-                      color="primary"
-                      component={Link}
-                      to={`/take-survey/${survey.id}`}
-                    >
-                      Answer survey
-                    </Button>
-                  </CardActions>
-                </Card>
-              ))}
-            </Stack>
-          </Container>
-
+                    Answer survey
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+          </Stack>
+        </Container>
       </Box>
     </Stack>
   );
