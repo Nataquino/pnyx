53d9ce71642743685e388a74903442cacc9aae2b
Jerzeil <72914327+Jerzeil@users.noreply.github.com>
2024-12-27 17:10:42 +0800

    responsive login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -35,14 +35,13 @@ const LoginPage = () => {
         console.log(response.data); // Log the response data to the console
         if(username === 'admin' && password === 'admin'){
           navigate('/admin');
-        }else{
+        } else {
           if (response.data.status === "success") {
             navigate('/home'); // Redirect to the home page
           } else {
             alert(response.data.message); // Show the error message
           }
         }
-        
       } catch (error) {
         console.error('An error occurred:', error.message); // Log the error message to the console
         alert('An error occurred: ' + error.message);
@@ -55,137 +54,132 @@ const LoginPage = () => {
       sx={{
         backgroundColor: "skyblue",
         height: "100vh",
+        display: "flex",
         justifyContent: "center",
-        alignContent: "center",
+        alignItems: "center",
       }}
     >
-      <Stack
-        direction="row"
+      <Card
         sx={{
-          width: "80vw",
           display: "flex",
-          justifyContent: "center",
-          marginLeft: 19,
+          flexDirection: { xs: "column", sm: "row" },
+          width: { xs: "90%", sm: "80%", md: "70%" },
+          borderRadius: 2,
+          boxShadow: 3,
         }}
       >
-        <Card
+        <Box
           sx={{
-            width: "100vw",
+            backgroundColor: "white",
+            padding: 4,
             display: "flex",
+            flexDirection: "column",
             justifyContent: "center",
-            borderRadius: 15,
+            alignItems: "center",
+            width: { xs: "100%", sm: "50%" },
           }}
         >
           <Box
-            fullWidth
+            component="img"
             sx={{
-              backgroundColor: "white",
-              padding: "20px",
-              width: "30vw",
-              height: "81.3vh",
+              height: 120,
+              width: 120,
+              borderRadius: "50%",
+              marginBottom: 2,
+            }}
+            alt="Logo"
+            src={logo}
+          />
+          <Container
+            sx={{
               display: "flex",
               flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
+              alignItems: "flex-start",
+              width: "100%",
+              maxWidth: 500,
+              padding: 2,
             }}
           >
-            <Box
-              component="img"
-              sx={{
-                height: 150,
-                width: 150,
-                borderRadius: 50,
-                margin: 1,
-              }}
-              alt="Logo"
-              src={logo}
+            <Typography variant="body1">Username</Typography>
+            <TextField
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              sx={{ width: "100%", marginBottom: 2 }}
             />
-            <Container
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start", // Align items to the left
-                width: "80%", // Take the full width of the parent container
-                maxWidth: 500, // Set a maximum width for the container
-                padding: 3, // Add padding to the container
-              }}
-            >
-              <Typography>Username</Typography>
-              <TextField
-                value={username}
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-                sx={{ width: "100%" }} // Make the TextField take the full width of the container
-              />
-            </Container>
-            <Container
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start", // Align items to the left
-                width: "80%", // Take the full width of the parent container
-                maxWidth: 500, // Set a maximum width for the container
-              }}
-            >
-              <Typography>Password</Typography>
-              <TextField
-                type="password"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                sx={{ width: "100%" }} // Make the TextField take the full width of the container
-              />
-            </Container>
-            <Button
-              variant="contained"
-              onClick={handleSubmit}
-              sx={{
-                marginTop: 2,
-                backgroundColor: "#05B1BF",
-                width: 150,
-                marginBottom: 7,
-              }}
-            >
-              LOGIN
-            </Button>
-            <Typography>
-              Don't have an account?{" "}
-              <span
-                onClick={() => navigate("/sign-up")}
-                style={{ color: "#05B1BF" }}
-              >
-                Click here
-              </span>
-            </Typography>
-          </Box>
-
-          <Box
-          maxHeight={"200vh"}
-          fullWidth
+          </Container>
+          <Container
             sx={{
-              backgroundColor: "#12A1E6",
-              justifyContent: "center",
-              width: "100vh"
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              width: "100%",
+              maxWidth: 500,
+              padding: 2,
             }}
           >
-            <Box
-              component="img"
-              sx={{
-                marginTop: 9,
-                height: 300,
-                width: 800,
-              }}
-              alt="loginphoto"
-              src={loginphoto}
+            <Typography variant="body1">Password</Typography>
+            <TextField
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              sx={{ width: "100%", marginBottom: 3 }}
             />
-            <Box sx={{display:"flex", justifyContent: "center", marginTop: 2 }}>
-              <Typography sx={{fontFamily: "fantasy", fontSize: 70}}>SURVEY PLATFORM</Typography>
-            </Box>
-          </Box>
-        </Card>
-      </Stack>
+          </Container>
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            sx={{
+              backgroundColor: "#05B1BF",
+              width: "100%",
+              padding: 1.5,
+            }}
+          >
+            LOGIN
+          </Button>
+          <Typography variant="body2" sx={{ marginTop: 2 }}>
+            Don't have an account?{" "}
+            <span
+              onClick={() => navigate("/sign-up")}
+              style={{ color: "#05B1BF", cursor: "pointer" }}
+            >
+              Click here
+            </span>
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            backgroundColor: "#12A1E6",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 3,
+            width: { xs: "100%", sm: "50%" },
+          }}
+        >
+          <Box
+            component="img"
+            sx={{
+              maxHeight: 300,
+              width: "100%",
+              objectFit: "contain",
+              marginBottom: 3,
+            }}
+            alt="loginphoto"
+            src={loginphoto}
+          />
+          <Typography
+            sx={{
+              fontFamily: "fantasy",
+              fontSize: { xs: 40, sm: 50, md: 70 },
+              textAlign: "center",
+            }}
+          >
+            SURVEY PLATFORM
+          </Typography>
+        </Box>
+      </Card>
     </Paper>
   );
 };
